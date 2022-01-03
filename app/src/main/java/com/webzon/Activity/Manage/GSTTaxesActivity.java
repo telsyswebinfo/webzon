bf33b3507bdbe65638257df413b38094b8d031ed
telsyswebinfo <webzonservice@gmail.com>
2022-01-03 18:54:24 +0530

    added ali

--- app/src/main/java/com/webzon/Activity/Manage/GSTTaxesActivity.java
+++ app/src/main/java/com/webzon/Activity/Manage/GSTTaxesActivity.java
@@ -3,12 +3,21 @@ package com.webzon.Activity.Manage;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
-public class GSTTaxesActivity extends AppCompatActivity {
+import com.webzon.Activity.CustomActivity;
+import com.webzon.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class GSTTaxesActivity extends CustomActivity {
+    @BindView(R.id.btn_create_tax) Button btn_create_tax;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gsttaxes);
+        ButterKnife.bind(this);
+        setupActionBar("Tax",true);
     }
 }
