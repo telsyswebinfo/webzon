491c34ab7a61f1d7d90dd562f8af00034f971923
telsyswebinfo <webzonservice@gmail.com>
2022-01-06 14:50:30 +0530

    added ali

---
+++ app/src/main/java/com/webzon/Activity/Manage/RequestGSTActivity.java
@@ -0,0 +1,14 @@
+package com.webzon.Activity.Manage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RequestGSTActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_request_gstactivity);
+    }
+}
---
+++ app/src/main/java/com/webzon/Model/ModelITRList.java
@@ -0,0 +1,2 @@
+package com.webzon.Model;public class ModelITRList {
+}
